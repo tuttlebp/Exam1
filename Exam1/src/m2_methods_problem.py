c820a8f4968c85ab874d9bd8023539041547ad2d
tuttlebp <tuttlebp@rose-hulman.edu>
2019-02-01 11:35:55 -0500

    circles are cool

--- Exam1/src/m2_methods_problem.py
+++ Exam1/src/m2_methods_problem.py
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 1  READ the doc-string for the is_prime function defined below.
+# DONE: 1  READ the doc-string for the is_prime function defined below.
 # You do NOT need to understand its implementations,
 # just its specification (per the doc-string).
 # You should  ** CALL **  functions as needed in implementing the
@@ -58,7 +58,7 @@ def is_prime(n):
 
 def test_count_primes():
     ###############################################################################
-    # TODO: 2  READ the doc-string for the count_primes function below,
+    # DONE: 2  READ the doc-string for the count_primes function below,
     #   but DO NOT begin coding.
     # After you have READ this, change its _TODO_ to DONE.
     ###############################################################################
@@ -69,15 +69,26 @@ def test_count_primes():
     print('Actual count = ', count_primes(1, 3))
 
     ###############################################################################
-    #   TODO: 3. Add the additional test cases for this function below
+    #   DONE: 3. Add the additional test cases for this function below
     # After you have coded this, change its _TODO_ to DONE.
     ###############################################################################
     print()
     print('Your tests will go here!')
+
+    print('Test 2 of count primes')
+    print('m = 20 and n = 22')
+    print('Expected count = 0')
+    print('Actual count = ', count_primes(20, 22))
+
+    print('Test 1 of count primes')
+    print('m = 2 and n = 8')
+    print('Expected count = 4')
+    print('Actual count = ', count_primes(2, 8))
+
     return
 
 
-def count_primes(m,n):
+def count_primes(m, n):
     """
     #    What comes in:
     #      -- A positive integer m.
@@ -115,16 +126,23 @@ def count_primes(m,n):
     #     second and third examples above
     """
     ################################################################################
-    #   TODO: 4. Write the function count_primes(m,n) below here
+    #   DONE: 4. Write the function count_primes(m,n) below here
     # After you have coded and tested, change its _TODO_ to DONE.
     ################################################################################
+    if n > m:
+        count = 0
+        for k in range(m, n + 1):
 
-    return
+            if is_prime(k) is True:
+                print(k)
+                count = count + 1
+
+        return count
 
 
 def test_multiply_primes():
     ################################################################################
-    #   TODO: 6. Read the doc string for multiply_primes, below, then
+    #   DONE: 6. Read the doc string for multiply_primes, below, then
     #       then read the test cases here.
     #   After you have read both, change this _TODO_ to DONE.
     ###############################################################################
@@ -147,7 +165,7 @@ def test_multiply_primes():
     print('Actual output = ', multiply_primes(2, 8))
 
 
-def multiply_primes(m,n):
+def multiply_primes(m, n):
     """
        #    What comes in:
        #      -- A positive integer m.
@@ -172,6 +190,17 @@ def multiply_primes(m,n):
     #   TODO: 6. Write the function multiply_primes(m,n) below here
     # After you have coded and tested, change this _TODO_ to DONE.
     ###############################################################################
+    if n > m:
 
+        product = 1
+        for k in range(m, n+1):
+
+            if is_prime(k) is True:
+
+                product = product * k
+
+        return product
+# I tried a lot of ways to figure out a way around the special case trying to use if, elif, and else, but I was unable
+# to figure anything out. The code works for everything except when there is a no prime number for m to n.
 
 main()
